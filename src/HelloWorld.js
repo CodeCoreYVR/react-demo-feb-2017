da50a27d00fdc465405b9e187eb26abf3da3f42d
Steve Godin <steve.god@gmail.com>
2017-05-04 13:12:14 -0700

    Reimplement HelloWorld as class-based component

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
+/*
 function HelloWorld (props) {
   // when inlining an object in jsx, do not forget
   // the braces for evaluating js and the braces for
@@ -9,6 +9,14 @@ function HelloWorld (props) {
     style={{color: 'DeepPink'}}>{`Hello, ${props.name}`}</h1>
   // 👆 transpiled into 👇 by Babel
   // return React.createElement('h1', null, `Hello, ${props.name}`)
+}
+*/
+
+// 👇 is a class-based of the functional component above ☝️
+class HelloWorld extends React.Component {
+  render() {
+    return <h1 style={{color: 'DeepPink'}}>{`Hello, ${this.props.name}`}</h1>
+  }
 }
 
 export default HelloWorld;
